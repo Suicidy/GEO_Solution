9524eaf894bc1ae55aabc4124d2ad06e591a2437
Nine_Suicidy <phichayut.nine@gmail.com>
2018-01-12 00:22:21 +0700

    Searching php

--- N/html/Searching.php
+++ N/html/Searching.php
@@ -2,26 +2,30 @@
 define("servername", "localhost");
 define("username", "root");
 define("password", "");
+define("database","geo_db");
+define("charset","utf8");
 
 //$subject = $_POST["subject"];
 //$teacher_id = $_POST["id"];
 $subject = "MTH";
 $teacher_id = "58070501020";
+
 $array_result = array();
 
 function query($sql_statement){
-    $conn = mysqli_connect(servername, username, password);
+    $conn = mysqli_connect(servername, username, password, database);
+    mysqli_set_charset( $conn, charset);
     // Check connection
     if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+        die("Connection failed: " . mysqli_connect_error()) . "<br>";
     }
-    echo "Connected successfully";
+    echo "Connected successfully" . "<br>";
     $result = mysqli_query($conn, $sql_statement);
     mysqli_close($conn);
     return $result;
 }
 
-// Create connection
+// Create SQL Statement
 
 $sql = "SELECT  student.student_id, student.title, student.firstname, student.lastname 
 FROM student, course, assign_course
@@ -29,24 +33,15 @@ WHERE student.student_id = assign_course.student_id
 AND assign_course.course_id = course.course_id
 AND assign_course.attending_status = 0 
 AND now() BETWEEN DATE_ADD(course.start_time , INTERVAL 1 HOUR) AND DATE_ADD(course.end_time, INTERVAL 1 HOUR)
-AND  $teacher_id  = course.teacher_id 
-AND course.subject LIKE '" . $subject . "%' ;";
-
-/*$sql = "SELECT  student.student_id, student.title, student.firstname, student.lastname 
-FROM student, course, assign_course
-WHERE student.student_id = assign_course.student_id 
-AND assign_course.course_id = course.course_id
-AND assign_course.attending_status = 0 
-AND now() BETWEEN DATE_ADD(course.start_time , INTERVAL 1 HOUR) AND DATE_ADD(course.end_time, INTERVAL 1 HOUR)
-AND '58070501020' = course.teacher_id 
-AND course.subject LIKE 'MTH%';";*/
+AND  '$teacher_id'  = course.teacher_id 
+AND course.subject LIKE '$subject%' ;";
 
 $results = query($sql);
 
 while($result = mysqli_fetch_assoc($results)) {
     $array_result[] = $result;
-    echo $result[student_id] . $result[title] . $result[firstname] . $result[lastname] ;
 }
+echo json_encode($array_result, JSON_UNESCAPED_UNICODE);
 
-mysqli_free_result($resuls);
+mysqli_free_result($results);
 ?>
