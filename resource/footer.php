22205c78dc0c2f856c749d74beb1cb22b7001fc4
Looksorn <35325953+Looksorn@users.noreply.github.com>
2018-01-12 15:48:13 +0700

    Update footer.php

--- resource/footer.php
+++ resource/footer.php
@@ -10,7 +10,7 @@
             ติดต่อสอบถาม:
             <a class="nav-link" href="https://www.facebook.com/GEO.Solution.KMUTT/"><font color="white"><i class="fa fa-facebook-square" aria-hidden="true"></i>   GEO Solution</font></a>
             หรือ   สำนักงานการศึกษาสำหรับผู้มีความสามารถพิเศษ (GEO)<br>
-            <i class="fa fa-map-marker" aria-hidden="true"></i>   ตึก Learning Institute (LI) ชั้น 13<br>
+            <i class="fa fa-map-marker" aria-hidden="true"></i>   อาคาร Learning Institute (LI) ชั้น 13<br>
             <i class="fa fa-phone" aria-hidden="true"></i>   0-2470-8396
             </p>
           </div>
