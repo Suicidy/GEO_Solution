3c67587c26de94f84f16d6dedeb0eafe580585de
Looksorn <35325953+Looksorn@users.noreply.github.com>
2018-01-12 15:37:01 +0700

    Add files via upload

---
+++ resource/footer.php
@@ -0,0 +1,22 @@
+  <nav class="navbar navbar-inverse bg-inverse fixed-bottom">
+    <div class="nav nav-item">
+      <div class="container">
+        <div class="row">
+          <div class="col" align="right">
+            <img src="geo_logo2.png" style="align-content: right" class="d-inline-block align-top" alt="">
+          </div>
+          <div class="col">
+            <p class="navbar-text">
+            ติดต่อสอบถาม:
+            <a class="nav-link" href="https://www.facebook.com/GEO.Solution.KMUTT/"><font color="white"><i class="fa fa-facebook-square" aria-hidden="true"></i>   GEO Solution</font></a>
+            หรือ   สำนักงานการศึกษาสำหรับผู้มีความสามารถพิเศษ (GEO)<br>
+            <i class="fa fa-map-marker" aria-hidden="true"></i>   ตึก Learning Institute (LI) ชั้น 13<br>
+            <i class="fa fa-phone" aria-hidden="true"></i>   0-2470-8396
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</body>
+</html>
