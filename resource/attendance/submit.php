88bc2444c2d932f0512f9743c4f1935d09e46e0c
Nine_Suicidy <phichayut.nine@gmail.com>
2018-01-13 22:37:41 +0700

    Redirect to Home

--- resource/attendance/submit.php
+++ resource/attendance/submit.php
@@ -1,12 +1,15 @@
 <?php
+$redirect = '/geo_solution/index.php';
+header( "Location: $redirect");
 require $_SERVER['DOCUMENT_ROOT'].'/geo_solution/config.php';
-
-foreach($_POST['student_id'] as $student_id){
-    $course_id = $_POST['course_id'];
-    $sql = "UPDATE assign_course
-            SET attending_status = 1
-            WHERE student_id = '$student_id' AND course_id = '$course_id';";
-    query($sql);
+if (isset($_POST['student_id'])){
+    foreach($_POST['student_id'] as $student_id){
+        $course_id = $_POST['course_id'];
+        $sql = "UPDATE assign_course
+                SET attending_status = 1
+                WHERE student_id = '$student_id' AND course_id = '$course_id';";
+        query($sql);
+    }
 }
-
+exit;
 ?>
