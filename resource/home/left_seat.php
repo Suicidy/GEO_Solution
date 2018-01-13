e3b38a3eafbdbf45fd3a82ea446133fb9cbfb82e
Thanathip <thanathip.sunate@gmail.com>
2018-01-13 15:50:58 +0700

    left_seat

--- resource/home/left_seat.php
+++ resource/home/left_seat.php
@@ -1,35 +1,21 @@
 <?php
 	require "../../config.php";
 
-	$result = "SELECT course.course_id , count(assign_course.course_id) AS countSeat
+	$id = $_POST['course_id'];
+	$result = "SELECT course.course_id , course.max_seat - count(assign_course.course_id) AS leftSeat , course.max_seat
 	FROM assign_course RIGHT JOIN course ON assign_course.course_id = course.course_id
-	WHERE course.course_id = 1
+	WHERE course.course_id = $id
 	GROUP BY course.course_id;";
-        
-        
-        $sql=query($result);
-
-
-
+  
+    $sql=query($result);
     while($rs=mysqli_fetch_array($sql,MYSQLI_ASSOC))
         {   
 
-           $final = $rs['countSeat'];
+           $final = $rs['leftSeat'].'/'.$rs['max_seat'];
 
             //========================
         }
-     
-
         echo $final;
-
-
         mysqli_free_result($sql);
-        
-        // $MTH112 .= "]";
-        // $PHY102 .= "]";
-        // $CHM103 .= "]";
-
-        // echo json_encode($outp,JSON_UNESCAPED_UNICODE);
-        // mysqli_close($con);
 
 ?>
