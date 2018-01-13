2905032257f9bfc41524d3010c520d853a9f0d6f
Nine_Suicidy <phichayut.nine@gmail.com>
2018-01-13 22:57:21 +0700

    GG again

--- resource/attendance.php
+++ resource/attendance.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8" />
     <link href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Kanit:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-    <script src="../js/jquery-3.2.1.js"></script>
+    <script src="/geo_solution/js/jquery-3.2.1.js"></script>
     <script>
       $(document).ready(function(){
         $("#table,#send").hide();
@@ -44,8 +43,7 @@
                 $("#course").val(data[0]['course_id']);
                 $("#table,#send").slideDown();
               } 
-            },"json"); 
-            
+            },"json");        
           }
         });
       });
