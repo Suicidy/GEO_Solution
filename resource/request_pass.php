e7f3ca0525362f10eb2b7f81e44f65dd04cd9445
athakit <tanakrit_-@hotmail.com>
2018-01-13 13:17:54 +0700

    temp

--- resource/request_pass.php
+++ resource/request_pass.php
@@ -2,25 +2,20 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
     
-    require 'phpmailer/phpmailer/src/Exception.php';
-    require 'phpmailer/phpmailer/src/PHPMailer.php';
-    require 'phpmailer/phpmailer/src/SMTP.php';
-    // Include config file
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
+    require 'phpmailer/src/SMTP.php';
     require_once '../config.php';
-    //require_once 'autoload.php';
 
 
     // Define variables and initialize with empty values
-
     $username = "";
     $username_err = "";
 
     // Processing form data when form is submitted
-
     if($_SERVER["REQUEST_METHOD"] == "POST"){
 
         // Check if username is empty
-
         if(empty(trim($_POST["username"]))){
             $username_err = 'Please enter Student ID.';
         } else{
@@ -28,35 +23,28 @@
         }
 
         // Validate credentials
-
         if(empty($username_err)){
 
             // Prepare a select statement
-
             $sql = "SELECT student_id, email FROM student WHERE student_id = ?";
             if($stmt = mysqli_prepare($link, $sql)){
 
                 // Bind variables to the prepared statement as parameters
-
                 mysqli_stmt_bind_param($stmt, "s", $param_username);
 
                 // Set parameters
-
                 $param_username = $username;
 
                // Attempt to execute the prepared statement
-
                 if(mysqli_stmt_execute($stmt)){
 
                     // Store result
-
                     mysqli_stmt_store_result($stmt);
 
                     // Check if username exists
                     if(mysqli_stmt_num_rows($stmt) == 1){                    
 
                         // Bind result variables
-
                          mysqli_stmt_bind_result($stmt, $username,$email);
                          if(mysqli_stmt_fetch($stmt)){
                             $select=mysqli_query($link,"select student_id,email,password from student where student_id = '$username' and password =' '");
@@ -82,35 +70,32 @@
                                  $mail->CharSet =  "utf-8";
                                  $mail->IsSMTP();
                                     // enable SMTP authentication
-    $mail->SMTPAuth = true;                  
-    // GMAIL username
-    $mail->Username = "ennea.krone@gmail.com";
-    // GMAIL password
-    $mail->Password = "1k9r4o2n5e40";
-    $mail->SMTPSecure = "SSL";  
-    // sets GMAIL as the SMTP server
-    //$mail->Host = "smtp.gmail.com";
-    $mail->Host = 'smtp.gmail.com';
-    // set the SMTP port for the GMAIL server
-    $mail->Port = "465";
-    $mail->From='ennea.krone@gmail.com';
-    $mail->FromName='your_name';
-    $mail->AddAddress('tanakrit.k18373@gmail.com', 'reciever_name');
-    $mail->Subject  =  'Reset Password';
-    $mail->IsHTML(true);
-    $mail->Body    = 'Click On This Link to Reset Password '.$pass.'';
-    if($mail->Send())
-    {
-      echo "Check Your Email and Click on the link sent to your email";
-    }
-    else
-    {
-      echo "Mail Error - >".$mail->ErrorInfo;
-    }
-  }	
-
-
-                                
+                                $mail->SMTPAuth = true;                  
+                                // GMAIL username
+                                $mail->Username = "geo.kmutt@gmail.com";
+                                // GMAIL password
+                                $mail->Password = "admin0000";
+                                $mail->SMTPSecure = "ssl";  
+                                // sets GMAIL as the SMTP server
+                                //$mail->Host = "smtp.gmail.com";
+                                $mail->Host = 'smtp.gmail.com';
+                                // set the SMTP port for the GMAIL server
+                                $mail->Port = "465";
+                                $mail->From='geo.kmutt@gmail.com';
+                                $mail->FromName='GEO KMUTT';
+                                $mail->AddAddress($emailreal);
+                                $mail->Subject  =  '[GEO Soultion] RESET PASSWORD';
+                                $mail->IsHTML(true);
+                                $mail->Body    = '<H2>Hello '.$emailreal.'</H2><br>Here is the password you need to login your GEO Solution :<br><H3>'.$password.'</H3><br><br>if you want to reset password <br><H3>'.$link.'</H3>';
+                                if($mail->Send())
+                                {
+                                    echo 'Your mail has been sent successfully.';        
+                                }
+                                else
+                                {
+                                echo "Mail Error - >".$mail->ErrorInfo;
+                                }
+                              }	
                             }	
                             else
                             {
@@ -123,15 +108,12 @@
                         // Display an error message if username doesn't exist
                         $username_err = 'No account found with that username.';
                     }
-
                 } else{
                     echo "Oops! Something went wrong. Please try again later.";
                 }
             }
             // Close statement
-
             mysqli_stmt_close($stmt);
-
         }
     }
 
