d01773ea26ef97b80dc57e4d2e9b6e9951e0a8e8
Worrachote <vorachote.9@gmail.com>
2018-01-13 13:55:05 +0700

    edit img paths ofcomfirm page

--- resource/login/comfirm.php
+++ resource/login/comfirm.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<title>Confirm</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-	<link href="/css/comfirm.css" rel="stylesheet">
+	<link href="../../css/comfirm.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Kanit:400,500,600,700,800,900" rel="stylesheet">
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -13,11 +13,12 @@
 <body>
 	<center>
 		<div class="card text-white bg-dark mb-3" style="max-width: 35rem;">
-		  <center><img src="/image/check-mark-button.png"></center>
+		  <center><img src="../../image/check-mark-button.png"></center>
 		  <div class="card-body">
 		  	<h5 class="card-title">ส่งเรียบร้อย</h5>
 		    <p class="card-text">กรุณาตรวจสอบรหัสผ่านที่อีเมล์</p>
-				<p class="card-text"><?php 
+				<p class="card-text">
+				<?php 
 				session_start();
 				 if(!isset($_SESSION['email']) || empty($_SESSION['email'])){
 					
@@ -26,7 +27,8 @@
 							}
 				echo $_SESSION['email'];
 				session_destroy();
-				?></p>
+				?>
+				</p>
 		    <button type="button" class="btn btn-primary">Home</button>
 		</div>
 </div>
