9add653b389a26a40e71dc365ea6044d6854ae9c
Looksorn <35325953+Looksorn@users.noreply.github.com>
2018-01-14 14:44:18 +0700

    แก้ๆ

--- resource/review.php
+++ resource/review.php
@@ -4,62 +4,23 @@
 	<meta charset="UTF-8">
 	<title>Review</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-	<link href="/css/review.css" rel="stylesheet">
+	<link href="css/review.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Kanit:400,500,600,700,800,900" rel="stylesheet">
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+	<script src="../js/show_student_course" type="text/javascript"></script>
+<!-- 	<script>
+$(document).ready(function(){
+    $("tr:even").css("background-color", "gray");
+});
+</script> -->
 </head>
 <body>
-	
 	<nav class="navbar navbar-light bg-light">
 	  <a class="navbar-brand" href="#">Navbar</a>
 	</nav>
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<h2>Review</h2>
-				<div class="form-group">
-					<select class="form-control col-2" style="float:left;" id="exampleFormControlSelect1">
-					    <option>ทั้งหมด</option>
-					    <option>ยังไม่ได้ทำการรีวิว</option>
-					    <option>ทำการรีวิวแล้ว</option>					    
-					</select>
-					<button type="submit" class="btn btn-success" id="button1">ยืนยัน</button>
-				</div>
-				<br>
-				<br>
-				<table class="table table-striped">
-				  <thead>
-				    <tr>
-				      <th scope="col"></th>
-				      <th scope="col">Course</th>
-				      <th scope="col">TA</th>
-				      <th scope="col"></th>
-				      <th scope="col">สถานะ</th>
-				    </tr>
-				  </thead>
-				  <tbody>
-				    <tr>
-				      <th scope="row">1</th>
-				      <td>
-				      	<p>Block Diagram</p>
-				      	<p>17/1/18</p>
-				  	  </td>
-				      <td>
-						<div class="square">
-							<img src="/image/team-member-2.jpg">
-							<p class="nickname">พี่lalala</p>
-						</div>
-				      </td>
-				      <td>
-						<p>ชื่อ ieiieieieieieieie</p>
-				      </td>
-				      <td>
-						<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-						  Review
-						</button>
-						<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 						  <div class="modal-dialog" role="document">
 						    <div class="modal-content">
 						      <div class="modal-header">
@@ -105,34 +66,32 @@
 						    </div>
 						  </div>
 						</div>
-				      </td>
-				    </tr>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h2>Review</h2>
+				<div class="form-group">
+					<select class="form-control col-2" style="float:left;" id="exampleFormControlSelect1">
+					    <option>ทั้งหมด</option>
+					    <option>ยังไม่รีวิว</option>
+					    <option>รีวิวแล้ว</option>					    
+					</select>
+					<button type="submit" class="btn btn-success" id="button1">ยืนยัน</button>
+				</div>
+				<br>
+				<br>
+				<table class="table table-striped">
+				  <thead>
 				    <tr>
-				      <th scope="row">2</th>
-				      <td>
-				      	<p>Block Diagram2222</p>
-				      	<p>11/1/18</p>
-				  	  </td>
-				      <td>				      	
-						<div class="square">
-							<img src="/image/team-member-2.jpg">
-							<p class="nickname">พี่lala</p>
-						</div>
-				      </td>
-				      <td>
-						<p>ชื่อ ieiieieieieieieie</p>
-				      </td>
-				      <td>
-				      	<button type="button" class="btn btn-secondary">Review</button>
-				      </td>					 			   
+				      <th scope="col">ลำดับ</th>
+				      <th scope="col">วิชา</th>
+				      <th scope="col">เรื่อง</th>
+				      <th scope="col">ผู้สอน</th>
+				      <th scope="col">รายละเอียด</th>
+				      <th scope="col">สถานะ</th>
 				    </tr>
-				    <tr>
-				      <th scope="row">3</th>
-				      <td>Larry</td>
-				      <td>the Bird</td>
-				      <td>@twitter</td>
-				    </tr>
-				  </tbody>
+				  </thead>
+				  <tbody id="student-course-list"></tbody>
 				</table>
 			</div>
 		</div>
