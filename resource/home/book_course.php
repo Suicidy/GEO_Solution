7cf28ba480cdaf8677883e22a44d7d8ca1c0fff7
Thanathip <thanathip.sunate@gmail.com>
2018-01-14 18:33:27 +0700

    book_course

--- resource/home/book_course.php
+++ resource/home/book_course.php
@@ -3,7 +3,7 @@
 	require $_SERVER['DOCUMENT_ROOT'].'/geo/geo_solution/config.php';
 	$courseid=8;
 	session_start();
-	$_SESSION["username"] = "58070501090";
+	$_SESSION["username"] = "58070501023";
 
 	$sql=query(" SELECT course.course_id , count(assign_course.course_id) AS countSeat, course.max_seat
                  FROM assign_course RIGHT JOIN course ON assign_course.course_id = course.course_id
@@ -16,15 +16,33 @@
       	$currentSeat = $rs['countSeat'];
       	$maxSeat = $rs['max_seat'];
       }
-    echo $currentSeat.','.$maxSeat;
+    
      // mysqli_free_result($sql);
-
-      $sql = query(" INSERT INTO `assign_course` (`course_id`, `student_id`, `comment_id`, `attending_status`, `star`, `time_stamp`) 
+    if($currentSeat<$maxSeat)
+    {
+    	$sql = query(" INSERT INTO `assign_course` (`course_id`, `student_id`, `comment_id`, `attending_status`, `star`, `time_stamp`) 
       				 VALUES ('$courseid', '$_SESSION[username]', NULL, '', NULL, CURRENT_TIMESTAMP);
 				  ");
 
-      echo $sql
 
+		if($sql==1){
+			echo 'Successfull';
+			}
+
+		else{
+
+			echo 'Booking Error';
+
+			}
+
+    }
+    else{
+
+    	echo 'This course is full already.';
+
+    }
+
+    
 
 
 
