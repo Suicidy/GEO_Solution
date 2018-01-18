5a2850760bd2d8dfc4581f186717677f60a2d3dc
Nine_Suicidy <phichayut.nine@gmail.com>
2018-01-18 16:08:00 +0700

    submit +find avg

--- resource/review/submit_review.php
+++ resource/review/submit_review.php
@@ -12,17 +12,26 @@ $array_result = array();
 
 //SQL Statement
 
-$sql = "SELECT comment_id, star
-        FROM assign_course
+$sql = "SELECT assign_course.comment_id, assign_course.star, course.teacher_id
+        FROM assign_course, course
         WHERE assign_course.course_id = '$course_id' 
-        AND assign_course.student_id = '$id';";
+        AND assign_course.student_id = '$id'
+        AND assign_course.course_id = course.course_id;";
 
 $results = query($sql);
 $result = mysqli_fetch_assoc($results);
 $comment_id = $result['comment_id'];
 $star_old = $result['star'];
+$teacher_id =  $result['teacher_id'];
+
 if ($star_old == NULL){
     $sql = "UPDATE assign_course SET star = '$star' WHERE student_id = '$id' AND course_id = '$course_id'; ";
+    $sql .= "UPDATE teacher SET star = (SELECT AVG(assign_course.star) 
+    FROM assign_course, course 
+    WHERE assign_course.star IS NOT NULL 
+    AND assign_course.course_id = course.course_id 
+    AND course.teacher_id = '$teacher_id'
+    GROUP BY course.teacher_id) WHERE teacher_id = '$teacher_id';"; 
 }
 else{
     $sql = "Nothing can query rn";
