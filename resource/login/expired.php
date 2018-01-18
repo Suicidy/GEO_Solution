885709edde9aab7c4a4e12ce528b23caef54a940
PrapasiriBonus <35325963+PrapasiriBonus@users.noreply.github.com>
2018-01-18 20:41:41 +0700

    uuuu

--- resource/login/expired.php
+++ resource/login/expired.php
@@ -14,15 +14,17 @@
 	<?php require_once '../header.php'; ?>
 	<link href="../../css/comfirm.css" rel="stylesheet">
 	<center>
-		<div class="card text-white bg-dark mb-3" style="max-width: 35rem;">
+		<!-- <div class="card text-white bg-dark mb-3" style="max-width: 35rem;"> -->
+		<div class="card wrapper" style="border: none; max-width: 35rem;">
 		  <center><img src="../../image/cancel.png"></center>
 		  <div class="card-body">
 		  	<h5 class="card-title">Error</h5>
 		    <p class="card-text">กรุณาทำการตรวจสอบอีกครั้ง</p>
 		    <button type="button" class="btn btn-primary">Home</button>
+			</div>
 		</div>
 	</center>
-</div>
+
 <!-- 
 </body>
 </html> -->
