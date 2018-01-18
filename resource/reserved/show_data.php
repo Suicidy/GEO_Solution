5948e5d424621a8d81e1aa35230697edf206bd34
Nine_Suicidy <phichayut.nine@gmail.com>
2018-01-18 20:44:47 +0700

    ready to commit reserved

--- resource/reserved/show_data.php
+++ resource/reserved/show_data.php
@@ -7,12 +7,13 @@ $array_result = array();
 
 //SQL Statement
 
-$sql = "SELECT course.subject, course.topic, teacher.nickname, course.start_time, course.course_id
+$sql = "SELECT course.subject, course.topic, teacher.nickname, course.start_time, course.course_id, 
+        (SELECT IF(now() < DATE_SUB(DATE(course.start_time),INTERVAL 2 DAY),1,0)) AS button_status
         FROM course, teacher, assign_course 
         WHERE course.course_id = assign_course.course_id 
         AND course.teacher_id = teacher.teacher_id 
         AND assign_course.attending_status = 0
-        AND now() < DATE_SUB(course.start_time , INTERVAL 1 HOUR)
+        AND now() < course.start_time
         AND assign_course.student_id = '$id';";
 
 $results = query($sql);
