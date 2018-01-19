7432e4632df17ff2e777f75becf3b61b263cd0ca
athakit <tanakrit_-@hotmail.com>
2018-01-20 01:17:47 +0700

    span data review

---
+++ js/check_review.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    // $.post("/geo_solution/resource/review/view_type.php",{},function(data,status){
+    //     type = data['type'];
+    //     if (type != "ta"){
+    //       window.location.replace("/geo_solution/index.php");
+    //     }
+    //     // else{ 
+    //     //   show_data("all"); 
+    //     // }
+    //   },"json");
+
+    $("#search").click(function(){
+    var id = $("#teacher_id").val();
+    var subject = $("#subject").val();
+    $.post("/geo_solution/resource/check_review/searching.php",{id : id,subject : subject},function(data,status){
+        $("#body").empty();
+        if (!jQuery.isEmptyObject(data)) {
+            for (var i = 0; data[i]; i++) {
+                var detail = "<div class='row'><div class='col-md-4 col-xs-6 class-name'><span>"+data[i]["topic"]+"</span></div><div class='col-md-4 col-xs-6 text-md-center room'><span>"+data[i]["start_time"]+"</span></div>";
+                var button1 = "<div class='col-md-4 col-xs-4 button-time'><center> <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#Review'><span>review</span></button>";
+                var button2 = "&nbsp<button type='button' class='btn btn-success' data-toggle='modal' data-target='#Comment'><span>comment</span></button></center></div></div>	<hr class='course-line'>";
+                $("#body").append(detail+button1+button2);
+              }
+          }
+    },"json")
+    //location.reload();
+    });
+});
