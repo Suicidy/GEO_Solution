4bc45ec42bb04fd09a4ee2e3fcbc89f6f6ca78c9
athakit <tanakrit_-@hotmail.com>
2018-01-19 16:28:19 +0700

    index new

---
+++ index2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/main.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">                                  
+
+</head>
+<body class="loading">
+<div id="wrapper">
+    <div id="bg"></div>
+    <div id="overlay"></div>
+    <div id="main">
+        <!-- Header -->
+            <header id="header" >
+            <title>GEO Solution</title>
+                <h1>GEO SOLUTION</h1>
+                <h2>GEO@KMUTT</h2>
+                <br><a href="/geo_solution/index.php" class="btn btn-dark btn-lg active" role="button" aria-pressed="true">Enter Site</a>
+                
+            </header>
+            
+    </div>
+</div>
+<script>
+    window.onload = function() { document.body.className = ''; }
+    window.ontouchmove = function() { return false; }
+    window.onorientationchange = function() { document.body.scrollTop = 0; }
+</script>
+</body>
+</html>
+
+
+
+
