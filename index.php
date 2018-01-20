72cee1d06467951d0c218c355419941457218805
PattharapongD <norjinz007@gmail.com>
2018-01-20 14:12:53 +0700

    js

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
             <header id="header" >
             <title>GEO Solution</title>
                 <h1>GEO SOLUTION</h1>
-                <h2>GEO@KMUTT</h2>
+             <h2>GEO@KMUTT</h2>
                 <br><a href="/geo_solution/home.php" class="btn btn-dark btn-lg active" role="button" aria-pressed="true">Enter Site</a>
                 
             </header>
@@ -32,7 +32,4 @@
 </script>
 </body>
 </html>
-
-
-
 
